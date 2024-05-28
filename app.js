aab7ab65b37da2a55f95aeec3ef7c6ae27064bee
jmc1503 <jake.mcguckin@hotmail.co.uk>
2024-05-28 22:30:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -167,42 +167,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
         markers = [];
 
-        fetch('data.json')
-            .then(response => response.json())
-            .then(data => {
-                const height = parseInt(document.getElementById('height').value);
-                const country = countrySelect.value;
-                const themePark = themeParkSelect.value;
+                    fetch('data.json')
+                        .then(response => response.json())
+                        .then(data => {
+                            const height = parseInt(document.getElementById('height').value);
+                            const country = countrySelect.value;
+                            const themePark = themeParkSelect.value;
 
-                let filteredRides = data.filter(item => {
-                    return (country === '' || item.Country === country) &&
-                        (themePark === '' || item['Theme Park'] === themePark) &&
-                        item['Minimum Height'] <= height &&
-                        item['Maximum Height'] >= height &&
-                        item.Active === 1; // Filter rides where Active = 1
+                            let filteredRides = data.filter(item => {
+                                return (country === '' || item.Country === country) &&
+                                    (themePark === '' || item['Theme Park'] === themePark) &&
+                                    item['Minimum Height'] <= height &&
+                                    item['Maximum Height'] >= height;
+                            });
+
+                            const themeParks = [...new Set(filteredRides.map(item => item['Theme Park']))];
+
+                            themeParks.forEach(park => {
+                                const parkData = data.find(item => item['Theme Park'] === park);
+                                if (parkData) {
+                                    const totalRidesInPark = data.filter(ride => ride['Theme Park'] === park).length;
+                                    const availableRidesInPark = filteredRides.filter(ride => ride['Theme Park'] === park).length;
+                                    const percentage = ((availableRidesInPark / totalRidesInPark) * 100).toFixed(0);
+
+                                    const marker = L.marker([parkData.Latitude, parkData.Longitude]).addTo(map);
+                                    marker.bindPopup(`<b>${park}</b><br>${parkData.Country}<br>${percentage}% of rides available`).openPopup();
+                                    markers.push(marker);
+                                }
+                            });
+                        })
+                        .catch(error => {
+                            console.error('Error fetching data:', error);
+                        });
                 });
-
-                const themeParks = [...new Set(filteredRides.map(item => item['Theme Park']))];
-
-                themeParks.forEach(park => {
-                    const parkData = data.find(item => item['Theme Park'] === park);
-                    if (parkData) {
-                        const totalRidesInPark = data.filter(ride => ride['Theme Park'] === park
-                        const parkData = data.find(item => item['Theme Park'] === park);
-                        if (parkData) {
-                            const totalRidesInPark = data.filter(ride => ride['Theme Park'] === park).length;
-                            const availableRidesInPark = filteredRides.filter(ride => ride['Theme Park'] === park).length;
-                            const percentage = ((availableRidesInPark / totalRidesInPark) * 100).toFixed(0);
-
-                            const marker = L.marker([parkData.Latitude, parkData.Longitude]).addTo(map);
-                            marker.bindPopup(`<b>${park}</b><br>${parkData.Country}<br>${percentage}% of rides available`).openPopup();
-                            markers.push(marker);
-                        }
-                    });
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                });
+            });
         });
     });
 });
